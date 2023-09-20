8d50b6c0b6963f6c0b73cc7a3a59c8a564a7c757
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-09-20 07:07:04 +0530

    Create SieveAlgorithm.cpp

---
+++ Learnings/SieveAlgorithm.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+using std::cin, std::cout, std::endl;
+
+int* Sieve(int n)
+{
+    int* arr=new int[n+1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        arr[i] = i;
+    }
+    arr[0] = 0;
+    arr[1] = 0;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (arr[i] != 0)
+        {
+            int j = i * i;
+            while (j <= n)
+            {
+                arr[j] = 0;
+                j += i;
+            }
+        }
+    }
+
+    return arr;
+}
+
+int main()
+{
+    cout<<"Calculate primes in a given range"<<endl;
+    int n;
+    cout << "Enter a positive numbers: ";
+    cin >> n;
+    int* prime = Sieve(n);
+    cout<<"Prime numbers:";
+    for (int i = 0; i < n + 1; i++)
+    {
+        if (prime[i] != 0)
+        {
+            cout<<prime[i]<<' ';
+        }
+    }
+    delete[] prime;
+    return 0;
+}
