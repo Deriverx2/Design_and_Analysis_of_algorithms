3fdf7af4764b3c5d55b6c18be2cf60193ce135f2
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-10-11 15:23:20 +0530

    Update quickSort.cpp

--- Learnings/quickSort.cpp
+++ Learnings/quickSort.cpp
@@ -1,57 +1,47 @@
-#include <iostream>
+#include<iostream>
+using std::cin,std::cout,std::endl,std::swap;
 
-using std::cin, std::cout, std::endl, std::swap;
-
-int partition(int *arr, int l, int r)
-{
-    int pivot = arr[r];
-    int i = l - 1;
-    for (int j = l; j <= r - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
+int pivot(int *arr, int l, int r){
+    int pivote=arr[r];
+    int i=l-1;
+    for (int j=r-1;j>l;j--){
+        if (arr[j]<pivote){
             i++;
-            swap(arr[i], arr[j]);
+            swap(arr[i],arr[j]);
         }
     }
-    swap(arr[i + 1], arr[r]);
-
-    return i + 1;
+    ++i;
+    swap(arr[i],arr[r]);
+    return i;
 }
 
-void quickSort(int *arr, int l, int r)
-{
-    if (l < r)
-    {
-        int pivot = partition(arr, l, r);
-        quickSort(arr, l, pivot - 1);
-        quickSort(arr, pivot+1, r);
+void quickSort(int *arr, int l, int r){
+    if (l<r){
+        int p=pivot(arr,l,r);
+        quickSort(arr,l,p-1);
+        quickSort(arr,p+1,r);
     }
 }
 
-int main()
-{
-    int N;
-    cout << "Enter number of Elements:";
-    cin >> N;
-    int arr[N];
-    cout << "Enter elements: ";
-    for (int i = 0; i < N; i++)
-    {
-        cin >> arr[i];
+int main(){
+    int n;
+    cout<<"Enter length of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array: ";
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    cout << "Before sort:";
-    for (int i = 0; i < N; i++)
-    {
-        cout << arr[i] << " ";
+    cout<<"Before sort: ";
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
-    quickSort(arr, 0, N - 1);
-    cout << "After sort:";
-    for (int i = 0; i < N; i++)
-    {
-        cout << arr[i] << " ";
+    cout<<endl;
+    quickSort(arr,0,n-1);
+    cout<<"After sort: ";
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
-
+    
     return 0;
 }
