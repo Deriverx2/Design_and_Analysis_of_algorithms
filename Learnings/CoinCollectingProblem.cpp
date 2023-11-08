fec9a653e5ba05194e59ed42307265490202367a
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-11-08 15:13:16 +0530

    Update CoinCollectingProblem.cpp

--- Learnings/CoinCollectingProblem.cpp
+++ Learnings/CoinCollectingProblem.cpp
@@ -9,47 +9,60 @@ int max(int a, int b)
     return b;
 }
 
-void printCoins(int** sum, int m, int n)
+void CoinPath(int** sum, int m, int n)
 {
-    if(m<1 || n<1)return;
-    if((sum[m][n])==sum[m-1][n]){
-        printCoins(sum,m-1,n);
-        cout<<m+1<<","<<n+1<<endl;
-    }else if((sum[m][n])==sum[m][n-1]){
-        printCoins(sum,m,n-1);
-        cout<<m+1<<","<<n+1<<endl;
-    }else if((sum[m][n])-1==sum[m-1][n-1]){
-        printCoins(sum,m-1,n);
-        cout<<m+1<<","<<n+1<<endl;
-    }else{
-        printCoins(sum,m,n-1);
-        cout<<m+1<<","<<n+1<<endl;
+    if (m==0){
+        while(n!=0){
+            cout<<"-->("<<m+1<<","<<n+1<<")";
+            n--;
+        }
+        cout<<"-->(1,1)";
+        return;
     }
+    if (n==0){
+        while(m!=0){
+            cout<<"-->("<<m+1<<","<<n+1<<")";
+            m--;
+        }
+        cout<<"-->(1,1)";
+        return;
+    }
+    cout<<m+1<<n+1<<endl;
+    if (sum[m][n]-1==sum[m][n-1]) CoinPath(sum,m,n-1);
+    else CoinPath(sum,m-1,n);
+    cout<<"-->("<<m+1<<","<<n+1<<")";
+    
     return;
 }
 
 void CCP(bool** coins, int m,int n)
 {
     int **sum=new int*[m];
-    for(int i = 0; i <m; i++)
+    for(int i = 0; i < m; i++){
         sum[i] = new int[n];
+    }
+    
     sum[0][0] = coins[0][0];
     for (int i = 1; i < n; i++)
     {
         sum[0][i] = sum[0][i-1] + coins[0][i];
     }
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < m; i++)
     {
         sum[i][0] = sum[i-1][0] + coins[i][0];
-        for (int j=1;j < m;j++){
+        for (int j=1;j < n;j++){
             sum[i][j]=max(sum[i-1][j],sum[i][j-1])+coins[i][j];
         }
     }
+    
     cout << "Largest value=" << sum[m-1][n-1] << endl;
-    printCoins(sum, m, n);
-    for(int i = 0; i <m; i++)
+    
+    CoinPath(sum, m-1, n-1);
+    
+    for(int i = 0; i <n; i++)
         delete sum[i];
     delete sum;
+    
     return;
 }
 
@@ -58,20 +71,22 @@ int main()
     int m,n;
     cout << "Enter dimension of board: ";
     cin >> m >> n;
-    // n = n + 1;
     bool **coins=new bool*[m];
-    for(int i = 0; i <m; i++)
+    for(int i = 0; i < m; i++)
         coins[i] = new bool[n];
+    
     cout<<"Enter 1 if coin is present,else zero"<<endl;
-    for (int i=0;i<m;i++){
-        for (int j=0;j<n;j++){
+    for (int i=0;i < m;i++){
+        for (int j=0;j < n;j++){
             cout<<"Location "<<i+1<<","<<j+1<<":";
             cin>>coins[i][j];
         }
     }
-    cout<<"Board"<<endl<<" ";
+    cout<<endl;
     
-    for (int i=0;i<m;i++){
+    
+    cout<<endl<<"\tBoard"<<endl<<" ";
+    for (int i=0;i<n;i++){
         cout<<" "<<i+1;
     }
     cout<<endl;
@@ -82,9 +97,12 @@ int main()
         }
         cout<<endl;
     }
+    
     CCP(coins,m,n);
+    
     for(int i = 0; i <m; i++)
         delete coins[i];
     delete coins;
+    
     return 0;
 }
