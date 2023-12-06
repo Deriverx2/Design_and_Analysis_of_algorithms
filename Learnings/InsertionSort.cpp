aee9f934d74e4503f796ee6b6646636a864f565e
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-12-06 18:36:58 +0530

    Update InsertionSort.cpp

--- Learnings/InsertionSort.cpp
+++ Learnings/InsertionSort.cpp
@@ -1,42 +1,48 @@
-#include<iostream>
-using std::cin,std::cout,std::endl;
+#include <iostream>
+using std::cin, std::cout, std::endl;
 
-
-void insertionSort(int* arr, int n){
-    for (int i = 1; i < n; i++) {
+void insertionSort(int *arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
         int key = arr[i];
-        int j = i-1;
-        while(j >= 0 && key < arr[j]){
-            arr[j+1] = arr[j];
+        int j = i - 1;
+        while (j >= 0 && key < arr[j])
+        {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1] = key;
+        arr[j + 1] = key;
     }
 }
 
-int main(){
-    
+int main()
+{
+
     int n;
-    cout<<"Enter size of array: ";
-    cin>>n;
+    cout << "Enter size of array: ";
+    cin >> n;
     int arr[n];
-    cout<<"Enter elements of array: ";
-    for (int i = 0; i < n; i++) {
-        cin>>arr[i];
+    cout << "Enter elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    
-    cout<<"Before sorting: ";
-    for (int i = 0; i < n; i++) {
-        cout<<arr[i]<<" ";
+
+    cout << "Before sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    
-    insertionSort(arr,n);
-    
-    cout<<"After sorting: ";
-    for (int i = 0; i < n; i++) {
-        cout<<arr[i]<<" ";
+    cout << endl;
+
+    insertionSort(arr, n);
+
+    cout << "After sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    
+
     return 0;
 }
